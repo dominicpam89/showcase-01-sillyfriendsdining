b05c6e240ded37f3c4fd39f7dacef8ab3aeb8479
dominicpam89 <dominicpam89@gmail.com>
2024-08-21 14:12:14 +0700

    add shadcn components configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 const App = () => {
 	return (
 		<>
 			<p className="text-5xl font-extrabold">Test Tailwind</p>
+			<Button variant="secondary">Test shadcn component</Button>
 		</>
 	);
 };
