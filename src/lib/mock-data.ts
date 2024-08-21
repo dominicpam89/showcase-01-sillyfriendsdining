5bf702199be0a196fbfcad34484fc859cd0dbb8d
dominicpam89 <dominicpam89@gmail.com>
2024-08-21 15:29:03 +0700

    add mock people in lib/

---
+++ src/lib/mock-data.ts
@@ -0,0 +1,28 @@
+export const mockpeople = [
+	{
+		id: 19237,
+		name: "Sarah Buldozer",
+		image: "https://i.pravatar.cc/150",
+		balance: 30,
+	},
+	{
+		id: 21097,
+		name: "George DC",
+		image: "https://i.pravatar.cc/150",
+		balance: 20,
+	},
+	{
+		id: 76931,
+		name: "Weather Guy",
+		image: "https://i.pravatar.cc/150",
+		balance: 7,
+	},
+	{
+		id: 86709,
+		name: "Unlimited Winston",
+		image: "https://i.pravatar.cc/150",
+		balance: 5,
+	},
+];
+
+export type MockPeopleType = (typeof mockpeople)[0];
