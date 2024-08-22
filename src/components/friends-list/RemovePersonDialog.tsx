68a4cb64a76570047841711abc38ccd0235972ca
dominicpam89 <dominicpam89@gmail.com>
2024-08-22 15:04:50 +0700

    update minor style

--- src/components/friends-list/RemovePersonDialog.tsx
+++ src/components/friends-list/RemovePersonDialog.tsx
@@ -2,10 +2,12 @@ import {
 	Dialog,
 	DialogContent,
 	DialogDescription,
+	DialogFooter,
 	DialogHeader,
 	DialogTitle,
 	DialogTrigger,
 } from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
 import { Trash2Icon as RemoveIcon } from "lucide-react";
 
 export default function RemovePersonDialog() {
@@ -15,13 +17,18 @@ export default function RemovePersonDialog() {
 				<RemoveIcon className="size-4 text-destructive" />
 			</DialogTrigger>
 			<DialogContent>
-				<DialogHeader>
+				<DialogHeader className="flex flex-col gap-2">
 					<DialogTitle>Are you absolutely sure?</DialogTitle>
 					<DialogDescription>
 						This action cannot be undone. This will permanently delete
 						your friend from the list
 					</DialogDescription>
 				</DialogHeader>
+				<DialogFooter className="w-full">
+					<Button variant="link" className="w-full">
+						I don't care, i don't think this person is my friend anyway!
+					</Button>
+				</DialogFooter>
 			</DialogContent>
 		</Dialog>
 	);
